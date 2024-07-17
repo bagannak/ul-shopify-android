12bc39844e27faa34d7f9809fd0d14924f7ae844
Baganna Kumbar <bagannak@testvagrant.com>
2024-07-17 17:29:26 +0530

    added locators of navigation icons present in home page(Home,Cart,Explore,Track,Profile)

--- ui/screens/common/homeScreen.ts
+++ ui/screens/common/homeScreen.ts
@@ -4,7 +4,17 @@ import { profile } from 'console';
 
 export class HomeScreen extends BaseScreen {
   private selectors = {
-    productLabel: { android: "//*[@text='PRODUCTS']", ios: "//*[@name='inp-fullname']" }
+    productLabel: { android: "//*[@text='PRODUCTS']", ios: "//*[@name='inp-fullname']" },
+    profileIconText:{android:"//*[@text='Profile']",ios:""},
+    profileIcon:{android:"com.ultralesson.ulshopify:id/icon-profile",ios:""},
+    homeIcon:{android:"com.ultralesson.ulshopify:id/icon-home"},
+    homeIconText:{android:"//*[@text='Home']"},
+    cartIcon:{android:"com.ultralesson.ulshopify:id/icon-cart"},
+    cartIconText:{android:"//*[@text='Cart']"},
+    exploreIcon:{android:"com.ultralesson.ulshopify:id/icon-explore"},
+    exploreIconText:{android:"//*[@text='Explore']"},
+    trackIcon:{android:"com.ultralesson.ulshopify:id/icon-track-order"},
+    trackIconText:{android:"//*[@text='Track']"}
   };
 
   async productLabelEle(): Promise<Element<'async'>> {
