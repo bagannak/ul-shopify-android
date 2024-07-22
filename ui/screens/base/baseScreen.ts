7de389e9af5736b27e43d182b16cd71fe7dc13df
Baganna Kumbar <bagannak@testvagrant.com>
2024-07-22 12:28:56 +0530

    modified baseScreen.ts

--- ui/screens/base/baseScreen.ts
+++ ui/screens/base/baseScreen.ts
@@ -258,11 +258,17 @@ export class BaseScreen {
     }
   }
   async scrollRight(startX:number,startY:number,endX:number){
-    await this.driver.touchAction([
-      { action: 'press', x: startX, y: startY },
-      { action: 'wait', ms: 1000 },
-      { action: 'moveTo', x: endX - startX, y: 0 },
-      { action: 'release' }
-    ]);
+    await this.driver.performActions([{
+      type: 'pointer',
+      id: 'finger1',
+      parameters: { pointerType: 'touch' },
+      actions: [
+        { type: 'pointerMove', duration: 0, x: startX, y: startY },
+        { type: 'pointerDown', button: 0 },
+        { type: 'pause', duration: 4000 },
+        { type: 'pointerMove', duration: 4000, x: endX, y: startY },
+        { type: 'pointerUp', button: 0 }
+      ]
+    }]);
   }
 }
